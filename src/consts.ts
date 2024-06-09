b86b4e828f6c443549117c8192888cee16590662
Samuel Leobon <76527824+ByggL@users.noreply.github.com>
2024-06-09 21:10:53 +0200

    minor infos

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Samuel Léobon ᛥ Big L";
+export const SITE_DESCRIPTION = "My all-in-one personal website, resume and portfolio";
+
+export const NAME = "Samuel Léobon";
+export const PSEUDO = "Big L";
